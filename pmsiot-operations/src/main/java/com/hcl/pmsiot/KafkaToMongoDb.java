6ebc519599d4ef8f59ca2cb369611aa64e6f44c3
manish.gour <manish.go@hcl.com>
2017-07-20 11:16:02 +0530

    Kafka to MongoDb and to Redis

--- pmsiot-operations/src/main/java/com/hcl/pmsiot/KafkaToMongoDb.java
+++ pmsiot-operations/src/main/java/com/hcl/pmsiot/KafkaToMongoDb.java
@@ -3,79 +3,100 @@ package com.hcl.pmsiot;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.regex.Pattern;
+import java.util.List;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.util.JSON;
+
 import kafka.serializer.StringDecoder;
 import scala.Tuple2;
 
 /**
- * Consumes messages from one or more topics in Kafka and does wordcount. Usage:
- * KafkaToMongoDb <brokers> <topics> <brokers> is a list of one or more Kafka
- * brokers <topics> is a list of one or more kafka topics to consume from
- *
- * Example: $ bin/run-example streaming.JavaDirectKafkaWordCount
- * broker1-host:port,broker2-host:port topic1,topic2
+ * Consumes messages from one or more topics in Kafka and does save data to
+ * mongodb.
+ * 
  * 
  * @author Manish gour
  * 
  */
 
 public final class KafkaToMongoDb {
-	private static final Pattern SPACE = Pattern.compile(" ");
+
+	public static String brokerHost = "localhost:9092";
+	public static String topics = "test";
+	public static String mongoHost = "192.168.2.68";
+	public static int mongoPort = 27017;
 
 	public static void main(String[] args) {
-		if (args.length < 2) {
-			System.err.println("Usage: JavaDirectKafkaWordCount <brokers> <topics>\n"
-					+ "  <brokers> is a list of one or more Kafka brokers\n"
-					+ "  <topics> is a list of one or more kafka topics to consume from\n\n");
-			System.exit(1);
-		}
 
-		// StreamingExamples.setStreamingLogLevels();
-
-		String brokers = args[0];
-		String topics = args[1];
-
-		// Create context with a 2 seconds batch interval
-		// Create context with a 2 seconds batch interval
 		SparkConf sparkConf = new SparkConf().setAppName("JavaDirectKafkaWordCount").setMaster("local[2]")
 				.set("spark.executor.memory", "1g");
+
+		// Create context with a 2 seconds batch interval
 		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
 
 		HashSet<String> topicsSet = new HashSet<String>(Arrays.asList(topics.split(",")));
 		HashMap<String, String> kafkaParams = new HashMap<String, String>();
-		kafkaParams.put("metadata.broker.list", brokers);
+		kafkaParams.put("metadata.broker.list", brokerHost);
 
 		// Create direct kafka stream with brokers and topics
 		JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(jssc, String.class, String.class,
 				StringDecoder.class, StringDecoder.class, kafkaParams, topicsSet);
 
 		// Get the lines, split them into words, count the words and print
-		JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
-			@Override
-			public String call(Tuple2<String, String> tuple2) {
-				return tuple2._2();
+		JavaDStream<String> data = messages.map(new Function<Tuple2<String, String>, String>() {
+			public String call(Tuple2<String, String> message) {
+				return message._2();
 			}
+
 		});
 
-		lines.foreachRDD(x -> {
-			x.collect().stream().forEach(n -> System.out.println("item of list: " + n));
+		data.foreachRDD(new VoidFunction<JavaRDD<String>>() {
+			@Override
+			public void call(JavaRDD<String> data) throws Exception {
+				Mongo mongo = new Mongo(mongoHost, mongoPort);
+				DB db = mongo.getDB("pmsdb");
+
+				DBCollection collection = db.getCollection("TestCollection");
+				if (data != null) {
+					List<String> result = data.collect();
+
+					for (String latLongs : result) {
+
+						System.out.println(latLongs);
+
+						String[] latLongArray = latLongs.split(",");
+
+						DBObject dbObject = (DBObject) JSON.parse(
+								"{'latitute':'" + latLongArray[0] + " ', 'longitude':'" + latLongArray[1] + "'}");
+
+						collection.insert(dbObject);
+						System.out.println("Inserted Data Done");
+					}
+				}
+
+			}
 		});
 
 		// Start the computation
 		jssc.start();
+
 		try {
 			jssc.awaitTermination();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
