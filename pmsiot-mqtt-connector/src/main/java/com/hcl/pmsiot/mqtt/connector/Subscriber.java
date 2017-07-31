24508a4ceb5b47b9ae4869a9b2ef5fca18cdfc47
Sachin Sharma <sachin_shar@HCL.COM>
2017-07-31 16:10:37 +0530

    removed main method

--- pmsiot-mqtt-connector/src/main/java/com/hcl/pmsiot/mqtt/connector/Subscriber.java
+++ pmsiot-mqtt-connector/src/main/java/com/hcl/pmsiot/mqtt/connector/Subscriber.java
@@ -5,7 +5,7 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class Subscriber {
 
-  public static void main(String[] args) throws MqttException {
+ /* public static void main(String[] args) throws MqttException {
 	String mqttClientUrl = "tcp://192.168.1.13:1883";
 	String topic = "iot_data";
     System.out.println("== START SUBSCRIBER ==");
@@ -16,6 +16,19 @@ public class Subscriber {
 
     client.subscribe(topic);
 
-  }
+  }*/
+  
+  public static void main() throws MqttException {
+		String mqttClientUrl = "tcp://192.168.1.13:1883";
+		String topic = "iot_data";
+	    System.out.println("== START SUBSCRIBER ==");
+
+	    MqttClient client=new MqttClient(mqttClientUrl, MqttClient.generateClientId());
+	    client.setCallback( new SimpleMqttCallBack() );
+	    client.connect();
+
+	    client.subscribe(topic);
+
+	  }
 
 }
--- pmsiot-sink/src/main/java/com/hcl/pmsiot/TestCassandraCLuster.java
+++ pmsiot-sink/src/main/java/com/hcl/pmsiot/TestCassandraCLuster.java
@@ -4,7 +4,26 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
 
 public class TestCassandraCLuster {
-	public static void main(String args[]) {
+	/*public static void main(String args[]) {
+
+		// Query
+		String query = "CREATE KEYSPACE tp WITH replication " + "= {'class':'SimpleStrategy', 'replication_factor':1};";
+
+		// creating Cluster object
+		Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+
+		// Creating Session object
+		Session session = cluster.connect();
+
+		// Executing the query
+		session.execute(query);
+
+		// using the KeySpace
+		session.execute("USE tp");
+		System.out.println("Keyspace created");
+	}*/
+	
+	public static void main() {
 
 		// Query
 		String query = "CREATE KEYSPACE tp WITH replication " + "= {'class':'SimpleStrategy', 'replication_factor':1};";
