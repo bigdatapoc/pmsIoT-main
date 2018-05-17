7470f1855ecd01673bcd9a9571aae876b91e2336
Rohan Ravindra Kandalkar <kandalakar.r@HCL.COM>
2018-05-17 16:30:17 +0530

    With modified UI

--- pmsiot-dashboard/src/main/resources/static/js/index.js
+++ pmsiot-dashboard/src/main/resources/static/js/index.js
@@ -6,17 +6,24 @@ $(document).ready(function(){
 	$('#btnSearch').click(function(){
     	//alert("In Search ");
 		
-		var search = $('#searchKey').val();
-
+		var search = $('#pac-input').val();
+		//alert(search);
+		
 		$.ajax({
         	type:"GET",
-            url:"http://localhost:8052/byid/"+search,
+            url:"/byid/"+search,
             success:function(data, textStatus, jqXHR){
-            mymap(data.latitude,data.longitude,data.userId);
-           	
+          // 	alert(data.latitude);
+            if(data!="")
+            	{
+            	  mymap(data.latitude,data.longitude,data.userId);
+            	}
+            else{
+            	  alert("NO RECORD FOUND");
+            	}
             },
 	         error:function( jqXhr, textStatus, errorThrown ) {           
-	        	 alert("In error ");
+	        	 alert("Please Enter SAP ID");
 	             
 	        }
         });
@@ -25,47 +32,22 @@ $(document).ready(function(){
 	
 
 	
-	
-	$('#btnallemployee').click(function(){
-    	//alert("In SUCCESS ");
-
-		$.ajax({
-        	type:"GET",
-            url:"http://localhost:8052/getallemployee",
-            success:function(data, textStatus, jqXHR){
-            document.getElementById('listid').style.display="none";
-            document.getElementById('listemp').style.display="block";
-            mymapallemp(data);
-            for(i=0; i<data.length; i++)
-			{
-            	//alert("In SUCCESS ");
-				$('#listemployee').append("<li>" + data[i].userId+ "   "+data[i].latitude+"   "+data[i].longitude+"</li>");
-				
-			}
-				             
-            },
-	         error:function( jqXhr, textStatus, errorThrown ) {           
-	        	 alert("In error ");
-	             
-	         }
-        });
-			
-	});
-	
 	function mymap(){
 		
 		  //alert("mymap");
 		  var latitude  = arguments[0];
 		  var longitude = arguments[1];
 		  var userId = arguments[2];
-		  var mapCanvas = document.getElementById("mapwithid");
+		  var mapCanvas = document.getElementById("map");
 		  var myCenter = new google.maps.LatLng(latitude,longitude); 
 		  var mapOptions = {center: myCenter, zoom: 20};
 		  var map = new google.maps.Map(mapCanvas,mapOptions);
+	
+
 		  var marker = new google.maps.Marker({
 		    position: myCenter,
-		    icon: "pinkball.png",
-		    //animation: google.maps.Animation.BOUNCE
+		    icon: "images/marker-person.png",
+	       // animation:google.maps.Animation.BOUNCE,
 		  });
 		  marker.setMap(map);
 		  
@@ -79,6 +61,7 @@ $(document).ready(function(){
 		 map.setCenter(marker.getPosition());
 		 });
 		  
+		 
 	}
 	
 	function mymapallemp(){
@@ -93,13 +76,21 @@ $(document).ready(function(){
 		      center: new google.maps.LatLng(latitude,longitude),
 		    });
 
+		 // Create the search box and link it to the UI element.
+	      /*  var input = document.getElementById('pac-input');
+	        var button = document.getElementById('btnSearch');
+	       // var searchBox = new google.maps.places.SearchBox(input);
+	        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+	        map.controls[google.maps.ControlPosition.TOP_LEFT].push(button);
+*/
+		    
 		    var infowindow = new google.maps.InfoWindow();
 		    var marker, i;
 		    for (i = 0; i < locations.length; i++) { 
 		      marker = new google.maps.Marker({
 		        position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
 		        icon: "images/marker-person.png",
-		        animation:google.maps.Animation.BOUNCE,
+		       // animation:google.maps.Animation.BOUNCE,
 		        map: map
 		      });
 
@@ -121,13 +112,13 @@ $(document).ready(function(){
 		//alert("fun");
     	$.ajax({
         	type:"GET",
-            url:"http://localhost:8052/allemployee",
+            url:"/allemployee",
             success:function(data, textStatus, jqXHR){
             mymapallemp(data);
            			             
             },
 	         error:function( jqXhr, textStatus, errorThrown ) {           
-	        	 alert("In error ");
+	        	 alert("IN ERROR");
 	             
 	         }
         });
